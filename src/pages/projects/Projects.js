84f1f536bc8b499cf4744494efae89b6c23d8e3e
gokkull-15 <gokkull04@gmail.com>
2025-08-07 15:19:43 +0530

    projects

--- src/pages/projects/Projects.js
+++ src/pages/projects/Projects.js
@@ -13,89 +13,96 @@ import landingpage from "../../assets/landingpage.png";
 import web3 from "../../assets/web3.png";
 import qr from "../../assets/qr.png";
 import chained from "../../assets/chained.png";
+import smartwill from "../../assets/smartwill.jpeg"
 
 function Projects() {
   const projects = [
     {
+      name: "SmartWill",
+      description: "A decentralized land registration platform with AI agent Support.",
+      link: "https://github.com/gokkull-15/ChainEdu.git",
+      image: smartwill,
+    },
+    {
       name: "ChainED",
       description: "A decentralized learning platform with AI assistant.",
       link: "https://github.com/gokkull-15/ChainEdu.git",
-      image: chained, // Example image
+      image: chained,
     },
     {
       name: "BlockInsure",
       description: "A decentralized platform to apply for the insurance.",
       link: "https://github.com/gokkull-15/Blockinsure-V1.git",
-      image: blockinsure, // Example image
+      image: blockinsure,
     },
     {
       name: "DevMa",
       description: "A decentalized platform that combines Devfolio and Luma.",
       link: "https://github.com/gokkull-15/devma-v1.git",
-      image: devma, // Example image
+      image: devma,
     },
     {
       name: "MediCO",
       description:
         "A platform to create interaction between the patient and doctor through onchain.",
       link: "https://github.com/gokkull-15/medico-app.git",
-      image: medico, // Example image
+      image: medico,
     },
     {
       name: "ShopX",
       description:
         "A decentralized platform for buying and selling ebooks through onchain.",
       link: "https://github.com/gokkull-15/shopx-app.git",
-      image: shopx, // Example image
+      image: shopx,
     },
     {
       name: "TRSA",
       description:
         "A social platform to organise events and view the skaters around the region.",
       link: "https://github.com/Sunil0881/TRSA.git",
-      image: trsa, // Example image
+      image: trsa,
     },
     {
       name: "Mahalakshmi build",
       description: "A real-estate side build for my client.",
       link: "https://github.com/gokkull-15/mahalakshmi-app.git",
-      image: mahalakshmi, // Example image
+      image: mahalakshmi,
     },
     {
       name: "Testimonial",
       description: "A website to get testimonials from my clients.",
       link: "https://github.com/gokkull-15/testimonials-app.git",
-      image: testimonial, // Example image
+      image: testimonial,
     },
     {
       name: "Imdb-next",
       description: "Cloned version of IMDB using nextjs.",
       link: "https://github.com/gokkull-15/imdb-next.git",
-      image: imdb, // Example image
+      image: imdb,
     },
     {
       name: "Wallet-connect",
       description: "A mini project to connect web3 wallet.",
       link: "https://github.com/gokkull-15/wallet-connect.git",
-      image: web3, // Example image
+      image: web3,
     },
     {
       name: "Landing Page",
       description: "A landing page for musical band for my client.",
       link: "https://github.com/gokkull-15/Landipage.git",
-      image: landingpage, // Example image
+      image: landingpage,
     },
     {
       name: "Qr code",
       description: "Mini project that generates qr code for the given links.",
       link: "https://github.com/gokkull-15/Qr-code.git",
-      image: qr, // Example image
+      image: qr,
     },
     {
       name: "Billing System",
       description: "A fully functioning billing system for my client.",
       link: "https://github.com/gokkull-15/billing-system-app.git",
-      image: billing, // Example image
+      image: billing,
     },
   ];
 
