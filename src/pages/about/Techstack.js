5d001757add77fdc6eb29d16821fd0befebad972
0xgokkull <gokkull04@gmail.com>
2026-01-04 18:21:54 +0530

    refactor: update Techstack to use IconGrid component
    - Replace inline icon grid with shared IconGrid
    - Add unique keys to each icon element
    - Simplify component to focus on icon configuration

--- src/pages/about/Techstack.js
+++ src/pages/about/Techstack.js
@@ -9,31 +9,23 @@ import {
 import { SiFirebase, SiNextdotjs, SiPostgresql } from "react-icons/si";
 import { FaRust } from "react-icons/fa";
 import { TbBrandTypescript } from "react-icons/tb";
+import IconGrid from "../../components/IconGrid";
 
 function Techstack() {
-  return (
-    <div className="flex justify-center flex-wrap gap-4 p-6">
-      {[
-        <DiReact />,
-        <SiNextdotjs />,
-        <DiJavascript1 />,
-        <TbBrandTypescript />,
-        <FaRust />,
-        <DiPython />,
-        <DiMongodb />,
-        <SiPostgresql />,
-        <SiFirebase />,
-        <DiGit />,
-      ].map((icon, index) => (
-        <div
-          key={index}
-          className="text-5xl lg:text-6xl p-3 text-cyan-200/90 hover:text-cyan-100 transition-colors"
-        >
-          {icon}
-        </div>
-      ))}
-    </div>
-  );
+  const icons = [
+    <DiReact key="react" />,
+    <SiNextdotjs key="next" />,
+    <DiJavascript1 key="js" />,
+    <TbBrandTypescript key="ts" />,
+    <FaRust key="rust" />,
+    <DiPython key="python" />,
+    <DiMongodb key="mongo" />,
+    <SiPostgresql key="postgres" />,
+    <SiFirebase key="firebase" />,
+    <DiGit key="git" />,
+  ];
+
+  return <IconGrid icons={icons} />;
 }
 
 export default Techstack;
