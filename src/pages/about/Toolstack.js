55041f5431e689b21144b4ffb7d30a1088f26427
0xgokkull <gokkull04@gmail.com>
2026-01-04 18:22:05 +0530

    refactor: update Toolstack to use IconGrid component
    - Replace inline icon grid with shared IconGrid
    - Add unique keys to each icon element
    - Consistent styling with Techstack component

--- src/pages/about/Toolstack.js
+++ src/pages/about/Toolstack.js
@@ -2,27 +2,19 @@ import React from "react";
 import { SiLinux, SiPostman, SiHeroku, SiCanva } from "react-icons/si";
 import { VscVscode } from "react-icons/vsc";
 import { IoLogoFigma } from "react-icons/io5";
+import IconGrid from "../../components/IconGrid";
 
 function Toolstack() {
-  return (
-    <div className="flex justify-center flex-wrap gap-4 p-6">
-      {[
-        <SiLinux />,
-        <SiPostman />,
-        <SiHeroku />,
-        <VscVscode />,
-        <IoLogoFigma />,
-        <SiCanva />,
-      ].map((icon, index) => (
-        <div
-          key={index}
-          className="text-5xl lg:text-6xl p-3 text-cyan-200/90 hover:text-cyan-100 transition-colors"
-        >
-          {icon}
-        </div>
-      ))}
-    </div>
-  );
+  const icons = [
+    <SiLinux key="linux" />,
+    <SiPostman key="postman" />,
+    <SiHeroku key="heroku" />,
+    <VscVscode key="vscode" />,
+    <IoLogoFigma key="figma" />,
+    <SiCanva key="canva" />,
+  ];
+
+  return <IconGrid icons={icons} />;
 }
 
 export default Toolstack;
